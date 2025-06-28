e714289c72d51d2989261ff3ebbaa40cc42a0318
josephjuma24 <josephjuma24@gmail.com>
2025-06-28 19:05:37 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+// Middleware to log requests
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 200,
+    message: 'Hello, World!'
+  })
+})
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080')
+})
